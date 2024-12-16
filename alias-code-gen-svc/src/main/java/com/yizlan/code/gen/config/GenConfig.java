ca667084c881349df67c25a54b11a2b0bf09962d
Zen Gershon <aiilary@163.com>
2024-12-16 22:45:45 +0800

    fix: fix cors

--- alias-code-gen-svc/src/main/java/com/yizlan/code/gen/config/GenConfig.java
+++ alias-code-gen-svc/src/main/java/com/yizlan/code/gen/config/GenConfig.java
@@ -20,6 +20,9 @@ import com.yizlan.code.gen.common.protocol.Result;
 import com.yizlan.gelato.canonical.protocol.TerResult;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * configuration for code generator service
@@ -34,4 +37,22 @@ public class GenConfig {
     public TerResult<String, String, Object> terResult() {
         return new Result<>();
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        /* 是否允许请求带有验证信息 */
+        corsConfiguration.setAllowCredentials(true);
+        /* 允许访问的客户端域名 */
+        corsConfiguration.addAllowedOriginPattern("*");
+        /* 允许服务端访问的客户端请求头 */
+        corsConfiguration.addAllowedHeader("*");
+        /* 允许访问的方法名,GET POST等 */
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
 }
